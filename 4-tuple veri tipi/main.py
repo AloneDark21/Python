53ae7ca9a906828bc33396620d763fbb07819ce9
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2024-02-03 02:22:00 +0300

    Create main.py

---
+++ 4-tuple veri tipi/main.py
@@ -0,0 +1,12 @@
+liste = ['a','b','c','d','e','a']
+print(liste)
+
+tup = ('a','b','c','d','e','a')
+print(tup)
+# tup un listten farkı normal normal parantez kullanmasıdır 
+# tup ile veri ataması yapılmaz
+
+print(tup.count('a')) #listede girdiğimiz elemandan kaç tane olduğunu bulan fonksiyon
+
+print(tup.index('e')) #girdiğimiz parametrenin listede kaçıncı indexte olduğunu bulan fonksiyon
+
