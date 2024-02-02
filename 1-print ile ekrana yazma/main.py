fd44bebf9fd07fc517ddefe4d72fcfd817dc63d0
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2024-02-03 02:16:21 +0300

    Create main.py

---
+++ 1-print ile ekrana yazma/main.py
@@ -0,0 +1,7 @@
+print("Kerem")
+
+#.format yapısı kullanma
+print("Benim Adim {0}, Yasim {1}".format("Kerem",23))
+
+#index kullanarak yazma
+print("Benim Adim {ad}, Yasim {yas}" .format(ad = "Kerem", yas = 23))
