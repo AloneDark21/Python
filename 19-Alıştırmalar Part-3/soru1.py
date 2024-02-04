5c88599fc253e8564157e7b36d180e7028cdae12
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2024-02-05 02:20:55 +0300

    Update soru1.py

--- 19-Alıştırmalar Part-3/soru1.py
+++ 19-Alıştırmalar Part-3/soru1.py
@@ -1,1 +1,8 @@
+# bir fonksiyon olusturun, 2 parametre alsın(uss ve taban), üss alma islemi yapsın ve sonucu return olarak döndürsün
 
+def uss_alma(a,b) :
+    return a**b
+taban = input("Taban degerini giriniz : ")
+uss = input("Uss degerini giriniz : ")
+
+print("{} ^ {} : {}".format(taban,uss,uss_alma(int(taban),int(uss))))
