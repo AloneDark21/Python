c511bc3b75bfae4c0f8a6f772cd33c7747af64e7
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2024-02-04 14:26:04 +0300

    Update range.py

--- 11-range, enumerate ve zip kullanımı/range.py
+++ 11-range, enumerate ve zip kullanımı/range.py
@@ -1,1 +1,8 @@
+# range kullanımı
 
+print(list(range(10))) # 0 dan 10 a kadar olan sayilari liste olarak yazdırır 
+
+range(5,20) # baslangic degerini de belirleyebiliriz
+
+for sayi in range(5,20) : # 5 ten 20 ye kadar olan sayılari ekrana yazdıralım
+    print(sayi)
