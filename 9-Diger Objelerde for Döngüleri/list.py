2d78eb72d430a08ab83aa829bd10bdc3922887d4
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2024-02-04 13:50:10 +0300

    Create list.py

---
+++ 9-Diger Objelerde for Döngüleri/list.py
@@ -0,0 +1,12 @@
+# listelerde for döngüsü kullanimi
+
+my_list = [[1, 2], [3, 4], [5, 6], [7, 8]]
+
+for x, y in my_list :# listeyi iç içe ekrana yazdırdık
+    print(x,y)
+
+print("\n")
+# liste elemanlari arasinda işlemler yapılabilir
+for x, y in my_list :
+    print(x*y)  # çarpma işlemi yaptik
+
