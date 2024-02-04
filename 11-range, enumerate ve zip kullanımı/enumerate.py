632726e33357bbd16d88de91ccf90452feb5694d
AloneDark21 <151954707+AloneDark21@users.noreply.github.com>
2024-02-04 14:25:47 +0300

    Update enumerate.py

--- 11-range, enumerate ve zip kullanımı/enumerate.py
+++ 11-range, enumerate ve zip kullanımı/enumerate.py
@@ -1,1 +1,10 @@
+# enumerate bir listedeki elemanları index degerleriyle beraber yazdırır
 
+harfler = ["a", "b", "c", "d"]
+
+for harf in enumerate(harfler) : # kullanmak istedigimiz yere yazıp kullanabiliriz
+    print(harf)
+
+# index degerlerine ve elemanlara ayrı ayrı da ulaşabiliriz    
+for index, harf in enumerate(harfler) :
+    print("{}. Harf : {}".format(index+1,harf))   
